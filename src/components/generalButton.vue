b884cc458f561649a5468889dab1770dffc581ae
AndreaChiari <chiariandrea94@gmail.com>
2023-01-31 13:21:36 +0100

    fine-header-add-general-button-component

---
+++ src/components/generalButton.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'generalButton'
+}
+</script>
+<template>
+    <button>
+        <slot></slot>
+    </button>
+
+
+</template>
+<style scoped lang="scss">
+@use '../assets/colors.scss' as *;
+
+button {
+    padding: 8px 30px;
+    background-color: $color-green;
+    border: none;
+    border-radius: 5px;
+    color: white;
+}
+</style>
---
+++ src/components/headerApp.vue
@@ -0,0 +1,127 @@
+<script>
+import generalButton from './generalButton.vue'
+export default {
+    name: 'headerApp',
+    components: { generalButton },
+    data() {
+        return {
+            headerNavList: [
+                'Home',
+                'Pages',
+                'Courses',
+                'Features',
+                'Blog',
+                'Shop'
+            ],
+
+        }
+    }
+
+
+
+
+}
+</script>
+<template>
+    <div class="background-header">
+        <header>
+            <div class="header-navbar d-flex justify-content-between align-items-center">
+                <img src="../assets/img/dark-logo.png" alt="dark-logo.png" class="img-fluid">
+                <ul class="d-flex">
+                    <li v-for="(list, i) in headerNavList" :key="i" class="me-4">{{ list }}
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li>
+                </ul>
+                <div class="d-flex header-icons">
+                    <i class="fa-brands fa-twitter me-4"></i>
+                    <i class="fa-brands fa-facebook me-4"></i>
+                    <i class="fa-brands fa-instagram me-4"></i>
+                    <i class="fa-brands fa-linkedin me-4"></i>
+                </div>
+            </div>
+            <div class="header-jumbotron pt-5 d-flex">
+                <div class="jumbo-info">
+                    <p> I'M WINGMAN</p>
+                    <h1 class="mb-3">Peace On Earth A Wonderful Wish But No Way</h1>
+                    <p class="mb-4">Success isn't overnight.It's when everyday you get a little bettter than the day
+                        before. It all
+                        adds up.</p>
+                    <generalButton>
+                        Get started from free
+                        <i class="fa-solid fa-arrow-right ms-1"></i>
+                    </generalButton>
+
+                </div>
+                <div class="jumbo-pic">
+                    <img src="../assets/img/home-movation-hero-image.png" alt="image.png">
+                </div>
+
+            </div>
+        </header>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+</template>
+<style scoped lang="scss">
+@use '../assets/mixin.scss' as *;
+
+// generals
+
+ul {
+    list-style: none;
+}
+
+.background-header {
+
+    background-image: url(../assets/img/background-pattern-wavify.png);
+}
+
+header {
+    @include standard-container;
+    min-height: 500px;
+
+    .header-navbar {
+        min-height: 100px;
+    }
+
+    .header-jumbotron {
+        min-height: 500px;
+
+        .jumbo-info {
+            flex-basis: 50%;
+
+            h1 {
+                padding-right: 250px;
+            }
+
+            p {
+                padding-right: 183px;
+            }
+        }
+
+        .jumbo-pic {
+            flex-basis: 50%;
+            background-image: url(../assets/img/home-movation-shape-face-300x267.png);
+            background-repeat: no-repeat;
+            background-position: 36% 137%;
+            background-size: 70%;
+
+
+            img {
+                height: auto;
+                width: 65%;
+            }
+
+        }
+    }
+}
+</style>
